67b8510e7daf3a8dc058baf68f66503fb1e28b82
kouchendi <koucxz@hotmail.com>
2019-09-30 18:56:42 +0800

    initialize

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import typescript from 'rollup-plugin-typescript';
+import pkg from './package.json';
+
+export default [
+	// browser-friendly UMD build
+	{
+		input: 'src/index.ts',
+		output: {
+			name: 'howLongUntilLunch',
+			file: pkg.browser,
+			format: 'umd'
+		},
+		plugins: [
+			resolve(),   // so Rollup can find node_modules
+			commonjs(),  // so Rollup can convert node_modules to an ES module
+			typescript(), // so Rollup can convert TypeScript to JavaScript
+		]
+	},
+
+	// CommonJS (for Node) and ES module (for bundlers) build.
+	{
+		input: 'src/index.ts',
+		external: ['ms'],
+		plugins: [
+			typescript(), // so Rollup can convert TypeScript to JavaScript
+		],
+		output: [
+			{ file: pkg.main, format: 'cjs' },
+			{ file: pkg.module, format: 'es' }
+		]
+	}
+];
+
